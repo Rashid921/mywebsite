7c106a34d7e7059ce4e683531b5885fc55a67951
Rashid Fathi Ege <Rashid_ege167@hotmail.co.uk>
2016-05-06 17:31:19 +0100

    website

--- chruchws/app.py
+++ chruchws/app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, request
+import sqlite3 as sql
+app = Flask(__name__)
 import os
 
 app = Flask(__name__)
@@ -23,6 +25,60 @@ def location():
 @app.route("/contactus")
 def contactus():
     return render_template("contactus.html")
+
+
+@app.route('/home')
+def home():
+   return render_template('home.html')
+
+@app.route('/enternew')
+def new_student():
+   return render_template('student.html')
+
+@app.route('/addrec',methods = ['POST', 'GET'])
+def addrec():
+   if request.method == 'POST':
+      try:
+         nm = request.form['nm']
+         addr = request.form['add']
+         city = request.form['city']
+         pin = request.form['pin']
+         
+         with sql.connect("databsae.db") as con:
+            cur = con.cursor()
+            
+            cur.execute("INSERT INTO students (name,addr,city,pin) VALUES (?,?,?,?)",(nm,addr,city,pin) )
+            
+            con.commit()
+            msg = "Record successfully added"
+      except:
+         con.rollback()
+         msg = "error in insert operation"
+      
+      finally:
+         return render_template("result.html",msg = msg)
+         con.close()
+
+@app.route('/list')
+def list():
+   con = sql.connect("databsae.db")
+   con.row_factory = sql.Row
+   
+   cur = con.cursor()
+   cur.execute("select * from students")
+   
+   rows = cur.fetchall();
+   return render_template("list.html",rows = rows)
+   
+
+
+
+
+
+
+
+
+
 
 if __name__ == '__main__':
     app.debug = True
